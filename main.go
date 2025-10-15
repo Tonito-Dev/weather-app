22c44ed7cb5b25495d7554dcf121b710baa45722
Tonito_Dev <mwasumbi2524@gmail.com>
2025-10-15 16:55:53 +0300

    Get data

--- main.go
+++ main.go
@@ -2,8 +2,23 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
 )
 
 func main() {
-	fmt.Println("hello weather")
+	API_key := "f36279aa4635403f869131542251510"
+	response, err := http.Get("http://api.weatherapi.com/v1/current.json?key=" + API_key)
+	if err != nil {
+		fmt.Print(err.Error())
+		os.Exit(1)
+	}
+
+	responseData, err := io.ReadAll(response.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(string(responseData))
 }
