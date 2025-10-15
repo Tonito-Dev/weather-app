a3d0adf37334453afd75cc03c07a4ef5e38c758a
Tonito_Dev <mwasumbi2524@gmail.com>
2025-10-15 18:28:12 +0300

    data struct

--- main.go
+++ main.go
@@ -8,6 +8,28 @@ import (
 	"os"
 )
 
+type Response struct {
+	location Location `json:"location"`
+	current  Current  `json:"current"`
+}
+
+type Location struct {
+	name    string `json:"name"`
+	country string `json:"country"`
+}
+
+type Current struct {
+	temp_c    int       `json:"temp_c"`
+	condition Condition `json:"condition"`
+	wind_kph  int       `json:"wind_kph"`
+	wind_dir  string    `json:"wind_dir"`
+	humidity  int       `json:"humidity"`
+}
+
+type Condition struct {
+	text string `json:"text"`
+}
+
 func main() {
 	API_key := "f36279aa4635403f869131542251510"
 	response, err := http.Get("http://api.weatherapi.com/v1/current.json?key=" + API_key + "&q=London&aqi=no")
